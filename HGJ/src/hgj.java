18502e89e0195eda9de7e78daeb921e7a7653cbb
huangguijia <74592343+huangguijia@users.noreply.github.com>
2020-12-01 17:20:11 +0800

    Update hgj.java

--- HGJ/src/hgj.java
+++ HGJ/src/hgj.java
@@ -1,4 +1,11 @@
 
 public class hgj {
+  [branch "master"] 
+	remote = origin 
+	merge = refs/heads/master
+[remote "origin"] 
+	url = https://github.com/huangbangyi2019/git_project.git 
+	fetch = +refs/heads/*:refs/remotes/origin/* 
+	push = refs/heads/master:refs/heads/master
 
 }
